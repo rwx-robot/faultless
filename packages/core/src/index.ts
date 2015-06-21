212cb3cdfaa21f57df8fa4a1b2fe2b0a6973e3b4
Faultless Team <johnnynode@gmail.com>
2015-06-21 10:03:54 +0800

    test(core): add comprehensive tests for index

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export * from './utils';
+export * from './errors';
+export * from './decorators';
+export * from './lifecycle';
+export * from './shutdown';
+export * from './production';
