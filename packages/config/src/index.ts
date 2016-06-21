9208e1d11cc55b16befa3564e936ce818ab48966
Faultless Team <johnnynode@gmail.com>
2016-06-21 13:34:54 +0800

    fix(config): resolve index edge cases

---
+++ packages/config/src/index.ts
@@ -0,0 +1,7 @@
+export * from './config';
+export * from './loader';
+export * from './schema';
+export * from './watchers';
+export * from './encryption';
+export * from './remote';
+export * from './versioning';
