edcb652a177b666ac1983921aff13708afbcafdb
Faultless Team <johnnynode@gmail.com>
2019-02-21 14:21:54 +0800

    fix(http): resolve index edge cases

---
+++ packages/http/src/index.ts
@@ -0,0 +1,24 @@
+export * from './types';
+export * from './server';
+export * from './router';
+export * from './middleware';
+export * from './request';
+export * from './response';
+export * from './decorators';
+export * from './interceptor';
+export * from './guard';
+export * from './pipe';
+export * from './filter';
+export * from './module';
+export * from './application';
+export * from './health';
+export * from './client';
+export type {
+  CorsOptions,
+  RequestIdOptions,
+  CompressionOptions,
+  RequestLoggerOptions,
+  SecurityHeadersOptions,
+  TimeoutOptions,
+  SizeLimitOptions,
+} from './middleware';
