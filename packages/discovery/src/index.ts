89573ef5ee25193cc28a0758595d95c2c4101e3a
Faultless Team <johnnynode@gmail.com>
2022-03-25 16:19:54 +0800

    chore(discovery): update index dependencies

---
+++ packages/discovery/src/index.ts
@@ -0,0 +1,6 @@
+export * from './service-registry';
+export * from './service-discovery';
+export * from './load-balancer';
+export * from './balancers';
+export * from './resolver';
+export * from './kubernetes';
